ef86140bc23b15f44809684105113c99f7f5d940
Mark Shipman <mark113@mapfreusa.com>
2023-12-10 12:35:08 -0500

    moved setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='funniest',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='http://github.com/sign-of-fourier/funniest',
+      author='Flying Circus',
+      author_email='markpshipman@yahoo.com',
+      license='MIT',
+      packages=['funniest'],
+      zip_safe=False)
