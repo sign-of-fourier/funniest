15e455137d5c885f5dfa5e93a127febef8bdc531
Mark Shipman <mark113@mapfreusa.com>
2023-12-13 16:40:02 -0500

    adding test to init

--- funniest/__init__.py
+++ funniest/__init__.py
@@ -1,3 +1,9 @@
 def joke():
     return (u'Wenn ist das Nunst\u00fcck git und Slotermeyer? Ja! ... '
             u'Beiherhund das Oder die Flipperwaldt gersput.')
+
+def test(msg):
+    if msg == 'Hello Mark':
+        return('How are you')
+    else:
+        return ("Hello Lavanya")
