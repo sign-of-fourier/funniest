64c911fba97dc920d8ad7cb0ef4d849cd7b0080d
mshipman <MARK113@mapfreusa.com>
2024-08-07 15:20:20 +0000

    testing directory structure

---
+++ funniest/joke2.py
@@ -0,0 +1,3 @@
+def mario():
+    return (u'Mario: how come no one says, there goes Mario the ship maker?'
+            u'just 1 sheep')
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='funniest',
-      version='0.1',
+      version='0.1.2',
       description='The funniest joke in the world',
       url='http://github.com/sign-of-fourier/funniest',
       author='Flying Circus',
